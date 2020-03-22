21158ff3a4f723b03da8d98c5bf668a321ba3393
Feiyu <cfyxfys@gmail.com>
2020-03-22 11:08:50 +0100

    2020.3.22
    Update

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,8 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+J = (1/m)*(log(sigmoid(theta'*X'))*(-y)-log(1-sigmoid(theta'*X'))*(1-y))+lambda*(sum(theta(2:end).^2))/(2*m);
+grad = (1/m)*((sigmoid(theta'*X')-y')*X) .+ [0,lambda/m*theta'(2:length(theta))];
 
 % =============================================================
 
